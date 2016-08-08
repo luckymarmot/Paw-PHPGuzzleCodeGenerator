9ffd2d8baa674ffc6241c1207934f233cb3e06fa
JonathanMontane <jon.montane@gmail.com>
2016-08-08 12:12:05 +0200

    improved release script to include LICENSE and README

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,9 @@ for file in $files
 do
     echo "working on folder -> $file"
     package=$(ls "./$file" | sed "s/.js$/$version.zip/");
+    echo "pacakge is $package";
+    cp "$base/README.md" "$base/dist/$file/";
+    cp "$base/LICENSE" "$base/dist/$file/";
     zip -r "$package" "$file/";
     mv "$package" "$base/releases/"
 done;
