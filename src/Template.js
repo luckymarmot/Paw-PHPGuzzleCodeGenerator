010662c37611530376160ec179788b6eda9f2a05
JonathanMontane <jon.montane@gmail.com>
2016-08-11 09:32:06 +0200

    escaped newline char for single request guzzle generator

--- src/Template.js
+++ src/Template.js
@@ -57,7 +57,7 @@ export default class Template {
             ::this.renderRequestObjects(this.props.requests) + `;
 
 $response = $client->send($request);
-echo "Response HTTP : " . $response->getStatusCode() . "\n";`
+echo "Response HTTP : " . $response->getStatusCode() . "\\n";`
     }
 
     renderMultipleRequests() {
